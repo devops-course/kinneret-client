1a69ee78677f03d1793063a7fe8ed145bf61af0b
Lilo <lilo@hp.com>
2015-04-05 12:55:34 +0300

    add and delete working, what about tests?

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,11 +2,19 @@ var myApp = angular.module('DemoApp', []);
 
 myApp.controller('HelloCtrl', function ($scope) {
 
-    $scope.tasks = ['Do something important', 'Another thing to do','Don\'t forget this'];
+    $scope.tasks = ['Do something important', 'Another thing to do', 'Don\'t forget this'];
 
-    $scope.addTask = function(newTaskText){
+    $scope.addTask = function (newTaskText) {
         $scope.tasks.push(newTaskText);
-    }
+        $scope.clearTaskText();
+    };
 
+    $scope.clearTaskText = function () {
+        $scope.newTaskText = '';
+    };
+
+    $scope.deleteTask = function (index) {
+        $scope.tasks.splice(index, 1);
+    };
 
 });
