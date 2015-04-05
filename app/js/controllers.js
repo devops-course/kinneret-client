23a9a19703b4c65e7510cf9e2a89cec53e0feef7
Lilo <lilo@hp.com>
2015-04-05 12:48:49 +0300

    now we can add tasks

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,5 +4,9 @@ myApp.controller('HelloCtrl', function ($scope) {
 
     $scope.tasks = ['Do something important', 'Another thing to do','Don\'t forget this'];
 
+    $scope.addTask = function(newTaskText){
+        $scope.tasks.push(newTaskText);
+    }
+
 
 });
