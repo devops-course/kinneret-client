602bef400349112993bc5ad4e703466cf52cb5c9
Lilo <lilo@hp.com>
2015-04-14 14:33:21 +0300

    start off without tasks

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,7 @@ var myApp = angular.module('DemoApp', []);
 
 myApp.controller('HelloCtrl', function ($scope) {
 
-    $scope.tasks = ['Do something important', 'Another thing to do', 'Don\'t forget this'];
+    $scope.tasks = [];
 
     $scope.addTask = function (newTaskText) {
         $scope.tasks.push(newTaskText);
