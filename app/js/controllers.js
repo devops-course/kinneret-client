12528642c70d703b1c3cb2aa130ec39a228545dc
elial <lilo@hp.com>
2015-05-07 00:31:03 +0300

    connected to local server!

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,25 @@
 var myApp = angular.module('DemoApp', []);
 
-myApp.controller('TaskListCtrl', function ($scope) {
+myApp.controller('TaskListCtrl', function ($scope, $http) {
 
     $scope.tasks = [];
+    $scope.busy = true;
+
+    function refresh() {
+        $http.get('/tasks').success(function (data, status, headers, config) {
+            $scope.tasks = data;
+            $scope.busy = false;
+        });
+    }
+
+    refresh();
 
     $scope.addTask = function (newTaskText) {
-        $scope.tasks.push(newTaskText);
+        //$scope.tasks.push(newTaskText);
+        $scope.busy = true;
+        $http.post('/tasks', {description: newTaskText}).success(function () {
+            refresh();
+        })
         $scope.clearTaskText();
     };
 
@@ -13,8 +27,33 @@ myApp.controller('TaskListCtrl', function ($scope) {
         $scope.newTaskText = '';
     };
 
-    $scope.deleteTask = function (index) {
-        $scope.tasks.splice(index, 1);
+    $scope.deleteTask = function (task) {
+        //$scope.tasks.splice(index, 1);
+        $scope.busy = true;
+        $http.delete('/tasks/' + task.id).success(function () {
+            refresh();
+        });
     };
 
+
 });
+
+/*
+
+ Server API
+
+ run the server:
+ in folder: server/kinneret-server/target
+ run the command: java -jar kinneret-server-1.0-SNAPSHOT.jar server ../kinneret-server.yml
+
+ list:
+ GET http://localhost:9000/tasks
+ add:
+ POST http://localhost:9000/tasks
+ with headers:
+ Content-Type: application/json
+ with payload:
+ {"description": "feed the octopus"}
+ remove:
+ DELETE http://localhost:9000/tasks/{id}
+ */
