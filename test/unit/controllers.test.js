0bd6378eac69b73f15d4157c5e82bb9d919d2d09
elial <lilo@hp.com>
2015-05-07 13:19:51 +0300

    unit tests fixed for e2e solution

--- test/unit/controllers.test.js
+++ test/unit/controllers.test.js
@@ -1,36 +1,56 @@
 'use strict';
 describe('TaskList Controller', function () {
-    var ctrl, scope;
+    var ctrl, scope, httpBackend;
 
 
     beforeEach(function () {
         module('DemoApp', function () {
         });
-        inject(function ($rootScope, $controller) {
+        inject(function ($rootScope, $controller, $httpBackend) {
+            httpBackend = $httpBackend;
             scope = $rootScope.$new();
             ctrl = $controller('TaskListCtrl', {$scope: scope});
+            httpBackend.expectGET('/tasks').respond(200, []);
+
         });
     });
+
+    function flushAndApply() {
+        httpBackend.flush();
+        scope.$apply();
+    }
 
     afterEach(function () {
         scope.$destroy();
     });
 
     it('should start with no tasks', function () {
+        flushAndApply();
         expect(scope.tasks).toBeDefined();
         expect(scope.tasks.length).toBe(0);
     });
 
     it('should add a new task', function () {
+        httpBackend.expectPOST('/tasks', {'description': 'hello world'}).respond(200);
+        httpBackend.expectGET('/tasks').respond(200, [{id: '999', description: 'hello world'}]);
         scope.addTask('hello world');
+        flushAndApply();
         expect(scope.tasks.length).toBe(1);
-        expect(scope.tasks[0]).toEqual('hello world');
+        expect(scope.tasks[0].description).toEqual('hello world');
+        expect(scope.tasks[0].id).toEqual('999');
     });
 
     it('should delete a task', function () {
+        httpBackend.expectPOST('/tasks', {'description': 'hello world'}).respond(200);
+        httpBackend.expectGET('/tasks').respond(200, [{id: '999', description: 'hello world'}]);
         scope.addTask('hello world');
+        flushAndApply();
         expect(scope.tasks.length).toBe(1);
-        scope.deleteTask(0);
+
+        httpBackend.expectDELETE('/tasks/999').respond(200);
+        httpBackend.expectGET('/tasks').respond(200, []);
+        scope.deleteTask(scope.tasks[0]);
+        flushAndApply();
         expect(scope.tasks.length).toBe(0);
     });
 
